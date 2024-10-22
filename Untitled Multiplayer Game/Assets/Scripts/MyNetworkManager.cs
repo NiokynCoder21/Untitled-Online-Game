4fb5de2aa650c7bda9ff9c653148b87aaaa1e9f9
NiokynCoder21 <tyalimpiofentse04@gmail.com>
2024-10-22 18:59:02 +0200

    done with mirror for now

--- Untitled Multiplayer Game/Assets/Scripts/MyNetworkManager.cs
+++ Untitled Multiplayer Game/Assets/Scripts/MyNetworkManager.cs
@@ -5,19 +5,38 @@ using Mirror;
 
 public class MyNetworkManager : NetworkManager
 {
-    public struct CharacterMessage : NetworkMessage
+    public GameObject PlayerPrefab2;  // Prefab for Player 2
+
+    public override void OnStartServer()
     {
-        public Race race;
-        public string name;
-        public Color glassesColor;
+        base.OnStartServer();
+        // We do not need to register any custom message handlers since the server decides
     }
 
-    public enum Race
+    // When a client connects, assign them a player prefab based on their connection order
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
-        None,
-        Black,
-        White,
-        Coloured,
-        Brown
+        GameObject gameobject;
+
+        if (numPlayers == 0)
+        {
+            // This is Player 1, spawn Prefab 1 (the default `playerPrefab`)
+            gameobject = Instantiate(playerPrefab);  // `playerPrefab` is from the NetworkManager
+            Debug.Log("Spawning Player 1 with Prefab 1");
+        }
+        else if (numPlayers == 1)
+        {
+            // This is Player 2, spawn Prefab 2
+            gameobject = Instantiate(PlayerPrefab2);  // Player 2 prefab
+            Debug.Log("Spawning Player 2 with Prefab 2");
+        }
+        else
+        {
+            Debug.LogError("More than 2 players connected!");
+            return;
+        }
+
+        // Add the player object to the connection
+        NetworkServer.AddPlayerForConnection(conn, gameobject);
     }
 }
